025341474d582c4be0bc1d586d3541aba34d9f1b
Nikolay Labinskiy <e-moe@ukr.net>
2014-12-13 16:02:14 +0200

    symlinks fix, ssd type check

--- classes/HotlineSsdParser.php
+++ classes/HotlineSsdParser.php
@@ -46,8 +46,7 @@ class HotlineSsdParser extends HotlineParser
             $hdd = new \stdClass();
             $hdd->type = trim($techCharArr[0]);
             $hdd->capacity = intval(preg_replace('/\D/u', '', $techCharArr[1]));
-            $interfaceArr = preg_split('/:/u', $techCharArr[2]);
-            $hdd->interface = trim($interfaceArr[1]);
+            $hdd->interface = trim($techCharArr[2]);
             $hdd->flashType = trim($techCharArr[3]);
 
             $titleUrlNodes = $xpath->query('.//div[@class="title-box"]/h3/a', $li);
