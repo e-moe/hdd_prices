7d9540ad9b25eac781e4c66831f65ea30ffce1be
Nikolay Labinskiy <e-moe@ukr.net>
2016-03-06 13:19:16 +0200

    bluma ui

--- public/ssd/index.php
+++ public/ssd/index.php
@@ -26,27 +26,27 @@ function drawTable($hdd)
             return;
         }
     }
-    echo '<table class="table table-condensed table-striped">';
+    echo '<table class="table is-striped is-narrow">';
     echo '<thead><tr><th>#</th><th>Title</th><th>Interface</th><th>Capacity (GB)</th><th>Avg Price</th><th>Ratio</th></tr></thead>';
     echo '<tbody>';
     foreach ($hdd as $id => $h): ?>
-    <tr>
-        <td><a href="?id=<?= $id ?>"><?= $id ?></a></td>
-        <td><a href="<?= $h->url ?>"><?= $h->title ?></a></td>
-        <td><?= $h->interface ?></td>
-        <td><?= $h->capacity ?></td>
-        <td>
+        <tr>
+            <td><a href="?id=<?= $id ?>"><?= $id ?></a></td>
+            <td><a href="<?= $h->url ?>"><?= $h->title ?></a></td>
+            <td><?= $h->interface ?></td>
+            <td><?= $h->capacity ?></td>
+            <td>
             <span title="From <?= $h->price->min ?> to <?= $h->price->max ?>">
                 <?= $h->price->avg ?>
             </span>
-        </td>
-        <td>
+            </td>
+            <td>
             <span title="From <?= $h->ratio->min ?> to <?= $h->ratio->max ?>">
                 <?= $h->ratio->avg ?>
             </span>
-        </td>
-    </tr>
-    <?php
+            </td>
+        </tr>
+        <?php
     endforeach;
     echo '</tbody>';
     echo '</table>';
@@ -54,51 +54,78 @@ function drawTable($hdd)
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>SSD Prices - Storage comparator</title>
-        <link href="/css/bootstrap.min.css" rel="stylesheet">
-        <!--[if lt IE 9]>
-          <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-          <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-        <![endif]-->
-    </head>
-    <body>
-        <div class="container">
-            <!-- Static navbar -->
-            <div class="navbar navbar-default" role="navigation">
-                <div class="container-fluid">
-                    <div class="navbar-header">
-                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                            <span class="sr-only">Toggle navigation</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-                        <a class="navbar-brand" href="/" data-instant><span class="glyphicon glyphicon-align-left"></span> Storage comparator</a>
-                    </div>
-                    <div class="navbar-collapse collapse">
-                        <ul class="nav navbar-nav">
-                            <li><a href="/hdd/">HDD</a></li>
-                            <li class="active"><a href="/ssd/">SSD</a></li>
-                        </ul>
-                        <ul class="nav navbar-nav navbar-right">
-                            <li><a href="http://github.com/e-moe/hdd_prices/"><span class="glyphicon glyphicon-file"></span> Github</a></li>
-                            <li><a href="http://www.labinskiy.org.ua/"><span class="glyphicon glyphicon-user"></span> Author</a></li>
-                        </ul>
-                    </div><!--/.nav-collapse -->
-                </div><!--/.container-fluid -->
-            </div>
-            <div class="page-header">
-                <h1>SSD prices <small>based on hotline.ua data</small></h1>
-            </div>
-            <?php drawTable(loadData('../../data/ssd/latest.json')); ?>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>HDD Prices - Storage comparator</title>
+    <link rel="stylesheet" href="css/font-awesome/font-awesome.min.css">
+    <link rel="stylesheet" href="css/bulma/bulma.min.css">
+</head>
+<body>
+<header class="header">
+    <div class="container">
+        <!-- Left side -->
+        <div class="header-left">
+                    <span class="header-item">
+                        <a class="button is-primary is-inverted" href="/">
+                            <span class="icon">
+                                <i class="fa fa-home"></i>
+                            </span>
+                            Storage comparator
+                        </a>
+                    </span>
+            <a class="header-item" href="/hdd">
+                HDD
+            </a>
+            <a class="header-item" href="/ssd">
+                <strong>SSD</strong>
+            </a>
         </div>
-        <script src="/js/jquery.min.js"></script>
-        <script src="/js/bootstrap.min.js"></script>
-        <script src="/js/instantclick.min.js" data-no-instant></script>
-        <script data-no-instant>InstantClick.init();</script>
-    </body>
+
+        <!-- Hamburger menu (on mobile) -->
+            <span class="header-toggle">
+                <span></span>
+                <span></span>
+                <span></span>
+            </span>
+
+        <!-- Right side -->
+        <div class="header-right header-menu">
+            <span class="header-item">
+                <a class="button is-primary is-inverted" href="https://github.com/e-moe/hdd_prices">
+                    <span class="icon">
+                        <i class="fa fa-github"></i>
+                    </span>
+                    GitHub
+                </a>
+            </span>
+        </div>
+    </div>
+</header>
+
+<section class="section">
+    <div class="container">
+        <div class="heading">
+            <h1 class="title">SSD prices</h1>
+            <h2 class="subtitle">
+                based on hotline.ua data
+            </h2>
+        </div>
+        <?php drawTable(loadData('../../data/ssd/latest.json')); ?>
+    </div>
+</section>
+
+<footer class="footer">
+    <div class="container">
+        <div class="content is-text-centered">
+            <p>
+                <strong>Storage comparator</strong> by <a href="//labinskiy.org.ua">Nikolay Labinskiy</a>.
+                The source code is licensed <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
+                The website content is licensed <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0</a>.
+            </p>
+        </div>
+    </div>
+</footer>
+</body>
 </html>
